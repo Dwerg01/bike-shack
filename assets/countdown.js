1ec29eb8a4f9f7e7ae90d9a4b49e823ab68c73f4
Dwerg01 <nardum@gmail.com>
2024-10-26 12:47:24 +0200

    section added for development in preview

--- assets/countdown.js
+++ assets/countdown.js
@@ -3,16 +3,25 @@ class CountDownTimer extends HTMLElement {
     super();
 
     //Grab required elements
+    this.days = this.querySelector(".days");
+    this.hours = this.querySelector(".hours");
+    this.minutes = this.querySelector(".minutes");
+    this.seconds = this.querySelector(".seconds");
 
-    // Set Date
-    this.endDateString = "May 25, 2026 16:37:52 EST";
+    this.endDateString = this.getAttribute("data-end-date")
+
+    // Set Date/
+    // this.endDateString = "May 25, 2026 16:37:52 EST";
     this.endDate = new Date(this.endDateString).getTime();
 
     // Start timer
+    this.timer = setInterval(this.handleTick.bind(this), 1000);
   }
 
   handleTick() {
     //logic and update elements
+    // console.log(this.endDate);
+
   }
 }
 
