09571b69f41f3ca8a3d81c86be5979c3031016e7
Dwerg01 <nardum@gmail.com>
2024-10-27 16:49:44 +0200

    countdown timer custom element created and implemented on Home Page

--- assets/countdown.js
+++ assets/countdown.js
@@ -3,15 +3,18 @@ class CountDownTimer extends HTMLElement {
     super();
 
     //Grab required elements
-    this.days = this.querySelector(".days");
-    this.hours = this.querySelector(".hours");
-    this.minutes = this.querySelector(".minutes");
-    this.seconds = this.querySelector(".seconds");
+    this.daysblock = this.querySelector(".days");
+    this.hoursblock = this.querySelector(".hours");
+    this.minutesblock = this.querySelector(".minutes");
+    this.secondsblock = this.querySelector(".seconds");
 
     this.endDateString = this.getAttribute("data-end-date")
 
+    this.countDownText = this.querySelector(".countdown-text");
     // Set Date/
     // this.endDateString = "May 25, 2026 16:37:52 EST";
+    // this.dateInput = thisquey.querySelector('[data-end-date]');
+    this.endDateString = this.querySelector('[data-end-date]').getAttribute('data-end-date');
     this.endDate = new Date(this.endDateString).getTime();
 
     // Start timer
@@ -19,10 +22,30 @@ class CountDownTimer extends HTMLElement {
   }
 
   handleTick() {
-    //logic and update elements
-    // console.log(this.endDate);
+      //logic and update elements
+    this.now = new Date().getTime();
+    this.timeleft = this.endDate - this.now;
 
+    if(this.timeleft < 0) {
+        this.countDownText.innerText = "THE SALE HAS ENDED!!!";
+        clearInterval(this.timer);
+        return;
+    }
+
+    this.msInDay = 1000 * 60 * 60 * 24;
+    this.msInHour = 1000 * 60 * 60;
+    this.msInMinute = 1000 * 60;
+
+    this.days = Math.floor(this.timeleft / this.msInDay);
+    this.hours = Math.floor((this.timeleft % this.msInDay) / this.msInHour);
+    this.minutes = Math.floor((this.timeleft % this.msInHour) / this.msInMinute);
+    this.seconds = Math.floor((this.timeleft % this.msInMinute) / 1000);
+    
+    this.daysblock.innerHTML = this.days + "d ";
+    this.hoursblock.innerHTML = this.hours + "h ";
+    this.minutesblock.innerHTML = this.minutes + "m ";
+    this.secondsblock.innerHTML = this.seconds + "s";
+    
   }
 }
-
 customElements.define("countdown-timer", CountDownTimer);
