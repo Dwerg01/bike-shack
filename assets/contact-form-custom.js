42baa48051b90226f162a70f1d78e6c2e0ae7f5f
Dwerg01 <nardum@gmail.com>
2025-08-04 12:38:08 +0200

    contact form updated

--- assets/contact-form-custom.js
+++ assets/contact-form-custom.js
@@ -1,1 +1,35 @@
 //logic for our custom contact form
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    const busAcc = document.getElementById('businessAccount');
+    const coInfo = document.getElementById('companyInfo');
+
+    const reasonForContact = document.querySelector('#contactReason');
+    const prodName = document.querySelector('#productNameField');
+    const orderNumDiv = document.querySelector('#orderNumberField');
+
+
+    reasonForContact.addEventListener('change', function() {
+        
+        prodName.classList.add('hidden');
+        orderNumDiv.classList.add('hidden');
+        if (reasonForContact.value == "product") 
+            {prodName.classList.remove('hidden')} 
+        else if (reasonForContact.value == "order") 
+            {(orderNumDiv.classList.remove('hidden'))} 
+
+    })
+
+    busAcc.addEventListener('change', function() {
+
+        busAcc.checked ? coInfo.classList.remove('hidden') : coInfo.classList.add('hidden');
+    })
+
+
+
+
+
+
+});
